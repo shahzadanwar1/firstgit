14f9a51bf8e24a15970e68560f215f584d1d39f7
shahzadanwar1 <116981613+shahzadanwar1@users.noreply.github.com>
2023-01-26 20:33:01 -0600

    initial files

---
+++ firstprojectnb/src/test/java/test/com/itexps/CalculatorTest.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+package test.com.itexps;
+
+import com.mycompany.edu.firstprojectnb.Calculator;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author mshan
+ */
+public class CalculatorTest {
+    Calculator cal;
+    
+    public CalculatorTest() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+        cal = new Calculator();
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+    @Test
+    public void testAdd(){
+        assertEquals(cal.add(2,4), 6);
+    }
+}
